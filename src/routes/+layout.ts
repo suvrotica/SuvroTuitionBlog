a405fd190c6ccdef9669ad32b46373e81d23a96f
suvrotica <suvrotica@gmail.com>
2025-07-23 11:11:56 +0530

    v19

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,2 +1,5 @@
 import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
+import { dev } from '$app/environment';
+import { injectAnalytics } from '@vercel/analytics/sveltekit';
 injectSpeedInsights();
+injectAnalytics({ mode: dev ? 'development' : 'production' });
