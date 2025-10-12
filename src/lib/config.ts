9077a2a300b901303349c7c718e62eecd3b083f3
Suvro Ghosh <117360356+suvrotica@users.noreply.github.com>
2025-10-12 10:54:29 +0530

    Update config.ts

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -3,4 +3,4 @@ export const SITE_TITLE = 'Suvro Ghosh';
 export const SITE_DESCRIPTION = 'A personal blog and portfolio for Suvro Ghosh, a full-stack engineer.';
 export const GITHUB_USERNAME = 'suvro-ghosh';
 
-export const POSTS_PER_PAGE = 10;
+export const POSTS_PER_PAGE = 100;
